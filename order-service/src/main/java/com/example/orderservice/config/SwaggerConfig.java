7bb631ec3636779b083034aefb1526f9d2dce2ee
YuJin Kwon <tokyj515@naver.com>
2025-03-31 11:26:59 +0900

    Fix: 스웨거 수정2

--- order-service/src/main/java/com/example/orderservice/config/SwaggerConfig.java
+++ order-service/src/main/java/com/example/orderservice/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.kt;
+package com.example.orderservice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
--- restaurant-service/src/main/java/com/example/restaurantservice/config/SwaggerConfig.java
+++ restaurant-service/src/main/java/com/example/restaurantservice/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.kt;
+package com.example.restaurantservice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
