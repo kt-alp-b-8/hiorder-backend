00dca7d8ab09696368ad034e49702eb013b6eabf
YuJin Kwon <tokyj515@naver.com>
2025-04-02 14:47:56 +0900

    feat: 레스토랑에 로그인 추가

--- restaurant-service/src/main/java/com/example/restaurantservice/repository/restaurant/RestaurantTableRepository.java
+++ restaurant-service/src/main/java/com/example/restaurantservice/repository/restaurant/RestaurantTableRepository.java
@@ -1,11 +1,13 @@
 package com.example.restaurantservice.repository.restaurant;
 
+import com.example.restaurantservice.entity.restaurant.Restaurant;
 import com.example.restaurantservice.entity.restaurant.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long> {
 
@@ -18,4 +20,9 @@ public interface RestaurantTableRepository extends JpaRepository<RestaurantTable
             "ORDER BY t.table_id ASC",
             nativeQuery = true)
     List<RestaurantTable> findByRestaurantIdOrderByTableIdAsc(@Param("restaurantId") Long restaurantId);
+
+
+    Optional<RestaurantTable> findRestaurantTableByRestaurantAndTableName(Restaurant restaurant, String tableName);
+
+//    findByRestaurant_RestaurantIdAndTableName -> 쿼리 만들기
 }
