7b875e5912da0d218c2a9037226c55a1b38b0bff
YuJin Kwon <tokyj515@naver.com>
2025-04-02 10:20:46 +0900

    feat: 로깅 추가, api 경로 수정

--- ocr-service/src/main/java/com/example/ocrservice/controller/OcrApiController.java
+++ ocr-service/src/main/java/com/example/ocrservice/controller/OcrApiController.java
@@ -5,7 +5,11 @@ import com.example.ocrservice.dto.OcrResponse;
 import com.example.ocrservice.service.OcrService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/ocr")
