1f237e04d5bdaea72425b8c0e436204ad641a221
KimSeongHo <kksshh0612@gmail.com>
2025-04-03 12:58:07 +0900

    feat : ocr 경로 변경

--- ocr-service/src/main/java/com/example/ocrservice/controller/OcrApiController.java
+++ ocr-service/src/main/java/com/example/ocrservice/controller/OcrApiController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 //@CrossOrigin(origins = "https://polite-pond-0844fed00.6.azurestaticapps.net")
 @RestController
 @RequestMapping("/ocr/api/ocr")
